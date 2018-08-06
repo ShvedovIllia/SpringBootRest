a7966e15296f6264d8f298c17123d6a37ce80b38
ShvedovIllia <shvedov.illia@gmail.com>
2018-08-06 17:51:30 +0300

    update repository

--- src/main/java/ua/logos/repository/BookRepository.java
+++ src/main/java/ua/logos/repository/BookRepository.java
@@ -3,13 +3,14 @@ package ua.logos.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import ua.logos.entity.Book;
 
 @Repository
 
-public interface BookRepository extends JpaRepository<Book, Long>{
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book>{
 
 
 //	@Query("select b from Book b where b.category.id = :categoryId")
