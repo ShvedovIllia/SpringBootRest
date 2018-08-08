22646453fd5d18efad56fe66ff72a4576fad1156
ShvedovIllia <shvedov.illia@gmail.com>
2018-08-08 15:07:38 +0300

    add sort

--- src/main/java/ua/logos/repository/UserRepository.java
+++ src/main/java/ua/logos/repository/UserRepository.java
@@ -3,13 +3,14 @@ package ua.logos.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import ua.logos.entity.User;
 
 @Repository
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User>{
 	
 	List<User> findByPostId(Long id);
 }
