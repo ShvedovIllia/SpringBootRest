f8961da0f3b53c34ee562788973cdb9c49950373
ShvedovIllia <shvedov.illia@gmail.com>
2018-08-10 19:36:19 +0300

    add exceptions

---
+++ src/main/java/ua/logos/exceptions/CustomizedResponseEntityExceptionsHandler.java
@@ -0,0 +1,5 @@
+package ua.logos.exceptions;
+
+public class CustomizedResponseEntityExceptionsHandler {
+
+}
---
+++ src/main/java/ua/logos/exceptions/models/BookNotFoundException.java
@@ -0,0 +1,5 @@
+package ua.logos.exceptions.models;
+
+public class BookNotFoundException {
+
+}
